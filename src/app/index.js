2acf4a678079e3a2df794fb5eceb449ad356e87a
Ashish <ash.ishu025g@gmail.com>
2018-07-11 22:26:49 +0530

    till 6 to conncet react redux

--- src/app/index.js
+++ src/app/index.js
@@ -31,26 +31,93 @@
 // render(<App />, window.document.getElementById('app'));
 
 // https://www.youtube.com/watch?v=ZKCYqJu4n3s&list=PL55RiY5tL51rrC3sh8qLiYHqUV3twEYU_&index=3
-import {createStore} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 
-const reducer = (state, action) => {
+const initialState = {
+    result: 1,
+    lastValues:[],
+    username:"Ashish"
+};
+
+const mathReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;            
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };              
+            break;
+        case "MULTIPLY":
+            state = {
+                ...state,
+                result: state.result * action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };            
             break;
     
         default:
             break;
     }
+    return state;
 }
 
-const store = createStore(reducer,1);
+const userReducer = (state = {
+    name:"Ashish",
+    age:29
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            state = {
+                ...state,
+                name: action.payload
+            };              
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };            
+            break;
+    
+        default:
+            break;
+    }
+    return state;
+}
+
+const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action", action);
+    next(action);
+};
+
+const store = createStore(combineReducers({mathReducer, userReducer}), {}, applyMiddleware(myLogger));
 
 store.subscribe(() => {
-    console.log("store updated!" + store.getState());
+    console.log("store updated!" + JSON.stringify(store.getState()));
 })
 
 store.dispatch({
     type:"ADD",
     payload:10
+});
+
+store.dispatch({
+    type:"ADD",
+    payload:111
+});
+
+store.dispatch({
+    type:"MULTIPLY",
+    payload:10
+});
+
+store.dispatch({
+    type:"SET_NAME",
+    payload:"Ashish Mishra"
+});
+
+store.dispatch({
+    type:"SET_AGE",
+    payload:30
 });
