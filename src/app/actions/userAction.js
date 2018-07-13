937b98ebee7c7a33f8db34fa53d17e3b0a71bfbd
Ashish <ash.ishu025g@gmail.com>
2018-07-13 21:53:32 +0530

    added redux call from api

--- src/app/actions/userAction.js
+++ src/app/actions/userAction.js
@@ -1,6 +1,7 @@
 //import { resolve } from "dns";
 //import { rejects } from "assert";
 import { setTimeout } from "timers";
+import { resolve } from "url";
 
 export function setName(name){
     // return dispatch =>{
@@ -12,13 +13,40 @@ export function setName(name){
     //     }
     //     ,3000);
     // }
+    // return{
+    //     type : "SET_NAME",
+    //     payload: new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve(name);
+    //         },3000)
+    //     })        
+    // }; 
     return{
         type : "SET_NAME",
-        payload: new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(name);
-            },3000)
-        })        
+        payload: {
+                promise: new Promise((resolve, reject) => { 
+                    fetch("http://localhost:8082/api/getdoctor", {
+                        method: 'GET',
+                        headers: {
+                          'Accept': 'application/json',
+                          'Content-Type': 'application/json'
+                        }
+                        //,body: JSON.stringify({ email: email, password: password})
+                    }
+                    ).then(                
+                        response => response.json(),
+                        error => console.log('An error occurred.', error)
+                    ).then(json => {
+                        // want to updatePath for the route here:
+                        //dispatch(updatePath('/'));
+                        resolve(json[6]["Desc"]);
+                    })
+                    .catch(error => {
+                        reject(error);
+                        //dispatch(error);
+                    })
+                })
+            }       
     }; 
 }
 
